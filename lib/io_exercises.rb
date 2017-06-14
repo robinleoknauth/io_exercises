1ad86df72ab192b77a03612d496c687c96feb579
LeoXugu <robinleoknauth@gmail.com>
2017-06-14 11:30:37 -0700

    guessing game

--- lib/io_exercises.rb
+++ lib/io_exercises.rb
@@ -10,3 +10,37 @@
 #   shuffles the lines, and saves it to the file "{input_name}-shuffled.txt". You
 #   could create a random number using the Random class, or you could use the
 #   `shuffle` method in array.
+
+def guessing_game
+  number = (1..100).to_a.sample
+  total_guesses = 0
+  guess = nil
+  until guess == number
+    guess = guess_number
+    miss(guess, number)
+    total_guesses += 1
+    puts "Number of guesses #{total_guesses}"
+  end
+  puts "The correct number was #{number}"
+  puts "It took you #{total_guesses} guesses in total to get to the right number"
+end
+
+def guess_number
+  puts "Please guess a number from 1 to 100"
+  guess = gets.chomp
+  puts "You guessed #{guess}"
+  guess.to_i
+
+end
+
+def miss(guess, num)
+  if guess < num
+    puts "You geussed too low"
+  else
+    puts "You guessed too high"
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  guessing_game
+end
